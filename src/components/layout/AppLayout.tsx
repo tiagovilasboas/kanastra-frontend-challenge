e8dbbbe96589ba3ba8793350e251a4a646b5a014
Tiago Vilas Boas <tcarvalhovb@gmail.com>
2025-08-04 08:20:11 -0300

    refactor: simplify AppLayout to use Sidebar without props

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,21 +1,15 @@
 import React from 'react'
 
 import { Header, Sidebar } from '@/components/layout'
-import { useNavigationStore } from '@/stores'
 
 interface AppLayoutProps {
   children: React.ReactNode
 }
 
 export const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
-  const { activeSection, setActiveSection } = useNavigationStore()
-
   return (
     <div className="app-layout">
-      <Sidebar
-        activeSection={activeSection}
-        onNavItemClick={setActiveSection}
-      />
+      <Sidebar />
       <div className="main-area">
         <Header onSearch={() => {}} />
         <main className="main-content">{children}</main>
