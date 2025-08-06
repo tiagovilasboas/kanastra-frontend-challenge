8856825eb5c3a8a27660ea0c3208aea5722392f8
Tiago Vilas Boas <tcarvalhovb@gmail.com>
2025-08-06 20:17:45 -0300

    Update stores with improved state management and navigation

--- src/stores/searchStore.ts
+++ src/stores/searchStore.ts
@@ -2,12 +2,17 @@ import { create } from 'zustand'
 
 interface SearchState {
   searchQuery: string
+  debouncedSearchQuery: string
   setSearchQuery: (query: string) => void
+  setDebouncedSearchQuery: (query: string) => void
   clearSearch: () => void
 }
 
 export const useSearchStore = create<SearchState>((set) => ({
   searchQuery: '',
+  debouncedSearchQuery: '',
   setSearchQuery: (query: string) => set({ searchQuery: query }),
-  clearSearch: () => set({ searchQuery: '' }),
-})) 
+  setDebouncedSearchQuery: (query: string) =>
+    set({ debouncedSearchQuery: query }),
+  clearSearch: () => set({ searchQuery: '', debouncedSearchQuery: '' }),
+}))
