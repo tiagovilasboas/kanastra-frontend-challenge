28864a556bb6b30754a319fad67d733032654925
Tiago Vilas Boas <tcarvalhovb@gmail.com>
2025-08-06 20:02:09 -0300

    feat: add main.tsx entry point for the application

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+import { AppProvider } from '@/app/providers/app-provider'
+import { router } from '@/app/router'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
+  </React.StrictMode>,
+)
